9e8aea8d11167ea5ae4cc3565ded9667286ffe38
Guilherme Ferreira <guikartman@gmail.com>
2019-05-10 20:56:43 -0300

    Começando interface

--- index.php
+++ index.php
@@ -7,12 +7,32 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Login</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <?php
-        // put your code here
-        echo 'Guilherme Dantas Ferreira';
-        ?>
-    </body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6">
+                    <form>
+                        <div class="form-group">
+                            <label><h3>Login</h3></label>
+                            <input type="text" name="usuario" class="form-control form-control-lg" >
+                        </div>                    
+                </div>
+                <div class="col-xl-6">
+                    <label><h3>Senha</h3></label>
+                    <input type="password" name="senha" class="form-control form-control-lg">
+                </div>
+                <input type="submit" class="btn btn-outline-primary btn-block btn-lg" name="enviar" value="ENTRAR"> 
+                </form>
+            </div>
+        </div>
+
+    </div>
+    <?php
+    // put your code here
+    ?>
+</body>
 </html>
