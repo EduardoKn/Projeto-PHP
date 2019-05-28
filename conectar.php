023e34fdf792e0b7284439cd4e556a97abea59f7
guikartman <guikartman@guikartman-300E5M-300E5L>
2019-05-28 20:11:43 -0300

    Confirmação de sessão

--- conectar.php
+++ conectar.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+session_start();
 
 $login = $_POST['usuario'];
 $lsenha = $_POST['senha'];
